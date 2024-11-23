ce2d789b47e06b2f3eb31322a8f04fc38eaec0c0
Gary Mejia <gmejiama@ucsc.edu>
2024-11-23 11:20:30 +0000

    Replacing source of codespace setup script with actual script itself

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,11 +1,17 @@
 #!/bin/sh -l
-source ../scripts/codespaces_setup.sh
+export PATH="$PATH:/tools/magic/bin"
+export PATH="$PATH:/tools/netgen/bin"
+export PATH="$PATH:/tools/ngspice/bin"
+export PATH="$PATH:/tools/xschem/bin"
+export PDK_ROOT="/tools/open_pdks/share/pdk"
+echo "PDK_ROOT=$PDK_ROOT"
+echo "PATH=$PATH"
 cd ..
 echo "Running LVS" >> $GITHUB_OUTPUT 
 make all_lvs
-if [ $? -neq  0]; then exit 1
+if [ $? -neq  0]; then exit 1 fi
 echo "Running SVG Generation" >> $GITHUB_OUTPUT
 make all_svg
-if [ $? -neq  0]; then exit 1
+if [ $? -neq  0]; then exit 1 fi
 echo "Success" >> $GITHUB_OUTPUT
 exit 0

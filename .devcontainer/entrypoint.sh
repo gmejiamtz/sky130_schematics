fe72dafef2055bacbe18d52e02e94f43ce423880
Gary Mejia <63254578+gmejiamtz@users.noreply.github.com>
2024-11-23 08:44:04 +0000

    Added workflow to try to run codespace docker image as an action and hoping build and release job actually runs this time

---
+++ .devcontainer/entrypoint.sh
@@ -0,0 +1,10 @@
+source ./codespaces_setup.sh
+cd ..
+echo "Running LVS"
+make all_lvs
+if [ $? -neq  0]; then exit 1
+echo "Running SVG Generation"
+make all_svg
+if [ $? -neq  0]; then exit 1
+echo "Success"
+exit 0

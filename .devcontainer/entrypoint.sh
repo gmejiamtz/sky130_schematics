072a05625fe560d41c14258a50a7ca504c9aa460
Gary Mejia <gmejiama@ucsc.edu>
2024-11-23 09:23:32 +0000

    Added steps to build docker in codespaces main action, added github output environment variables to entrypoint docker script

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,10 +1,11 @@
-source ./codespaces_setup.sh
+#!/bin/sh -l
+source ../scripts/codespaces_setup.sh
 cd ..
-echo "Running LVS"
+echo "Running LVS" >> $GITHUB_OUTPUT 
 make all_lvs
 if [ $? -neq  0]; then exit 1
-echo "Running SVG Generation"
+echo "Running SVG Generation" >> $GITHUB_OUTPUT
 make all_svg
 if [ $? -neq  0]; then exit 1
-echo "Success!"
+echo "Success!" >> $GITHUB_OUTPUT
 exit 0

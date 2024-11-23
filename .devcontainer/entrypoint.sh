a9d32a0dbe115fb5fa75ef8d8f5f03d6bb39e0bb
Gary Mejia <gmejiama@ucsc.edu>
2024-11-23 11:34:00 +0000

    Messing around with Dockerfile such that it only does the entrypoint, want to see it fail make properly by not having tools installed

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,17 +1,19 @@
 #!/bin/sh -l
-export PATH="$PATH:/tools/magic/bin"
-export PATH="$PATH:/tools/netgen/bin"
-export PATH="$PATH:/tools/ngspice/bin"
-export PATH="$PATH:/tools/xschem/bin"
-export PDK_ROOT="/tools/open_pdks/share/pdk"
-echo "PDK_ROOT=$PDK_ROOT"
+#export PATH="$PATH:/tools/magic/bin"
+#export PATH="$PATH:/tools/netgen/bin"
+#export PATH="$PATH:/tools/ngspice/bin"
+#export PATH="$PATH:/tools/xschem/bin"
+#export PDK_ROOT="/tools/open_pdks/share/pdk"
+#echo "PDK_ROOT=$PDK_ROOT"
 echo "PATH=$PATH"
+curr=pwd
+echo "$curr"
 cd ..
-echo "Running LVS" >> $GITHUB_OUTPUT 
+echo "Running LVS" >> "$GITHUB_OUTPUT"
 make all_lvs
 if [ $? -neq  0]; then exit 1 fi
-echo "Running SVG Generation" >> $GITHUB_OUTPUT
+echo "Running SVG Generation" >> "$GITHUB_OUTPUT"
 make all_svg
 if [ $? -neq  0]; then exit 1 fi
-echo "Success" >> $GITHUB_OUTPUT
+echo "Success" >> "$GITHUB_OUTPUT"
 exit 0
